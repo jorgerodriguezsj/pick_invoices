371ef1ecf92bff8006f5313c718b4038bfc8739e
Jorge Rodríguez <55578063+jorgerodriguezsj@users.noreply.github.com>
2022-02-24 10:33:40 +0100

    Add files via upload

---
+++ utils/entities_list.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# @Author: Jorge
+# @Created Time: 02/12/2020 9:34 PM
+
+Entities_list = [
+    "Sellers_name",
+    "Sellers_address",
+    "Sellers_VAT",
+    "Buyers_name",
+    "Buyers_address",
+    "Buyers_VAT",
+    "Invoice_number",
+    "Invoice_Date",
+    "Due_Date",
+    "Amount_Before_Taxes",
+    "VAT",
+    "Withholding_tax",
+    "Total_amount",
+    "iva_perc",
+    "wt_perc",
+]
